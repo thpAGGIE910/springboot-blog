9989a9b1de6a2a200668e05579c949eb4f35f738
Travis Payne <thpaggie910@gmail.com>
2017-11-02 11:47:50 -0500

    Add a `/posts` route that will show the posts index page

--- src/main/java/com/codeup/blog/controllers/PostsController.java
+++ src/main/java/com/codeup/blog/controllers/PostsController.java
@@ -1,4 +1,15 @@
 package com.codeup.blog.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class PostsController {
+
+    @GetMapping("/posts")
+    @ResponseBody
+    public String viewAllPosts() {
+        return "posts index page";
+    }
 }
