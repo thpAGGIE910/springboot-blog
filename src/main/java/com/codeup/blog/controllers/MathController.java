44cbf242ecf29964cbcb1738786aa7e017a02a04
Travis Payne <thpaggie910@gmail.com>
2017-11-02 11:21:52 -0500

    Create add route that returns the sum of two numbers passed as path variables

--- src/main/java/com/codeup/blog/controllers/MathController.java
+++ src/main/java/com/codeup/blog/controllers/MathController.java
@@ -1,8 +1,15 @@
 package com.codeup.blog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MathController {
-    
+    @GetMapping("/add/{num1}/and/{num2}")
+    @ResponseBody
+    public double add(@PathVariable double num1, @PathVariable double num2) {
+        return num1 + num2;
+    }
 }
