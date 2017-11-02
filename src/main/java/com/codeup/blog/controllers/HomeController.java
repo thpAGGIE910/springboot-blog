080706782999a45980b9c9801d1329e62388cf45
Travis Payne <thpaggie910@gmail.com>
2017-11-02 11:09:46 -0500

    Create a home controller for the route `/` which displays a welcome message

--- src/main/java/com/codeup/blog/controllers/HomeController.java
+++ src/main/java/com/codeup/blog/controllers/HomeController.java
@@ -1,4 +1,15 @@
 package com.codeup.blog.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String displayLandingPage() {
+        return "This is the landing page!";
+    }
 }
