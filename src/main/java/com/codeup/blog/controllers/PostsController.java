efb08ddb9ba2559df84d37795da7d5ae466ff5de
Travis Payne <thpaggie910@gmail.com>
2017-11-02 11:51:34 -0500

    Add \`/posts/{id}\` route that will display an individual post's details

--- src/main/java/com/codeup/blog/controllers/PostsController.java
+++ src/main/java/com/codeup/blog/controllers/PostsController.java
@@ -2,6 +2,7 @@ package com.codeup.blog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -11,5 +12,11 @@ public class PostsController {
     @ResponseBody
     public String viewAllPosts() {
         return "posts index page";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String getPostDetails(@PathVariable long id) {
+        return "view an individual post";
     }
 }
