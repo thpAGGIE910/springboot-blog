37b04c14a4eee28250e42075e8ed021741856586
Travis Payne <thpaggie910@gmail.com>
2017-11-03 14:54:57 -0500

    Edit `PostsController` class to return the views with required information for all posts and individual posts

--- src/main/java/com/codeup/blog/controllers/PostsController.java
+++ src/main/java/com/codeup/blog/controllers/PostsController.java
@@ -1,24 +1,47 @@
 package com.codeup.blog.controllers;
 
+import com.codeup.blog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+
 @Controller
 public class PostsController {
 
     @GetMapping("/posts")
-    //@ResponseBody
-    public String viewAllPosts() {
+    public String viewAllPosts(Model viewModel) {
+        ArrayList<Post> posts = new ArrayList<>();
+
+        Post post1 = new Post();
+        Post post2 = new Post();
+
+        post1.setTitle("Post 1");
+        post2.setTitle("Post 2");
+
+        post1.setBody("Example body for post 1.");
+        post2.setBody("Example body for post 2.");
+
+        posts.add(post1);
+        posts.add(post2);
+
+        viewModel.addAttribute("posts", posts);
+
         return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String getPostDetails(@PathVariable long id) {
-        return "view an individual post";
+    public String getPostDetails(@PathVariable long id, Model viewModel) {
+        Post post = new Post();
+        post.setTitle("Example Post");
+        post.setBody("This is the body of the example post");
+        viewModel.addAttribute("post", post);
+
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")
