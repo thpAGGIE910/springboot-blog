a6d664ee6114c019d66a302e2c657bf69b02448e
Travis Payne <thpaggie910@gmail.com>
2017-11-08 14:20:52 -0600

    Create a User class, with fields for id, username, email, and password

--- src/main/java/com/codeup/blog/models/User.java
+++ src/main/java/com/codeup/blog/models/User.java
@@ -1,5 +1,50 @@
 package com.codeup.blog.models;
 
-public class User {
+import javax.persistence.*;
 
+@Entity
+@Table(name = "users")
+public class User {
+    @Id @GeneratedValue private long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    public User() {}
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
