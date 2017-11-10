d8e7419f6a6f7d15d6690e8eeba7e608bd4aaf56
Travis Payne <thpaggie910@gmail.com>
2017-11-10 09:40:31 -0600

    Set up project to build a war file for deployment

--- src/main/java/com/codeup/blog/SpringbootBlogApplication.java
+++ src/main/java/com/codeup/blog/SpringbootBlogApplication.java
@@ -2,11 +2,18 @@ package com.codeup.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootBlogApplication {
+public class SpringbootBlogApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBlogApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringbootBlogApplication.class);
 	}
 }
